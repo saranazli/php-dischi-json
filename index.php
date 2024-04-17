1e3ce8dfae8bbb9b3613b66fd7169ab05d421e40
s4r44d43 <sararez@live.it>
2024-04-17 17:14:30 +0200

    [cicled] card

--- index.php
+++ index.php
@@ -8,7 +8,18 @@
 <body>
 
   <div class="container" id="app">
-    <h1>{{title}}</h1>
+    
+    <div 
+    v-for="(music, index_m) in disk"
+    :key= "index_m"
+    class="card" style="width: 18rem;">
+      <img :src="music.poster" class="card-img-top" alt="music">
+      <div class="card-body">
+        <h5 class="card-title">{{music.title}}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
   </div>
   
 
--- script.js
+++ script.js
@@ -5,8 +5,23 @@ createApp({
   data(){
     return{
       title: 'yo',
-      apiUrl: 'server.php'
+      apiUrl: 'server.php',
+      disk: []
     }
+  },
+  
+  methods: {
+
+    getApi(apiUrl){
+      axios.get(apiUrl).then(res => {
+        this.disk= res.data
+        console.log(this.disk)
+      })
+    }
+  },
+
+  mounted() {
+    this.getApi(this.apiUrl)
   }
 
 }).mount('#app');
